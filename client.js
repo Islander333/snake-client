2aec7c6a4d2c3ee0f83e782f692160e37d617802
Kyle Lindsay <kyle.lindsay333@gmail.com>
2024-06-15 12:03:22 -0700

    add broadcast msg for connection and player count

--- client.js
+++ client.js
@@ -1,34 +1,44 @@
 const net = require("net");
 const { IP, PORT } = require("./constants");
 
+let connection;
 
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: IP,
     port: PORT,
 
   });
-// interpret incoming data as text
+  // interpret incoming data as text
   conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    console.log(data);
-  });
 
   conn.on("connect", () => {
     console.log("Successfully connected to the game server");
-    conn.write("Name: ABC");
+    console.log("Use keys: WASD to Move!");
+    console.log("Use keys: 1, 2, 3 to chat!");
+    console.log("ctrl + c to exit!");
+    conn.write("Name: KL");
     
-   /*setInterval(() => {
-      conn.write("Move: up");
-    }, 50);
-    */
+    //broadcast data from server
+    conn.on("data", (data) => {
+      console.log(data);
+    });
+
+    //log player count
+    conn.on("playerCount", (count) => {
+      console.log(`Player: ${count} connected!`);
+    });
     
   });
 
-  
-
+  connection = conn;
   return conn;
 };
+//send messages from client to server for broadcasting
+const sendMessage = function(message) {
+  if (connection) {
+    connection.write(message);
+  }
+};
 
-module.exports = { connect };
+module.exports = { connect, sendMessage };
--- input.js
+++ input.js
@@ -2,7 +2,7 @@ const { MOVE_UP_KEY, MOVE_LEFT_KEY, MOVE_DOWN_KEY, MOVE_RIGHT_KEY } = require(".
 
 let connection;
 
-const setupInput = function (conn) {
+const setupInput = function(conn) {
   connection = conn;
   const stdin = process.stdin;  // create variable to hold the stdin object so we don't have to type process.stdin multiple times
   stdin.setRawMode(true); // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
@@ -11,32 +11,34 @@ const setupInput = function (conn) {
   stdin.on("data", handleUserInput);
   return stdin;   // return the stdin object so we can use it elsewhere in the program
 };
-//take ctrl c input and exit
-const handleUserInput = function (key) {
-  if (key ==='\u0003') {
+
+//user inputs
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    console.log("thanks for playing!");
     process.exit();
   }
-  if(key === MOVE_UP_KEY) {
+  if (key === MOVE_UP_KEY) {
     connection.write("Move: up");
   }
-  if(key === MOVE_LEFT_KEY) {
+  if (key === MOVE_LEFT_KEY) {
     connection.write("Move: left");
   }
-  if(key === MOVE_DOWN_KEY) {
+  if (key === MOVE_DOWN_KEY) {
     connection.write("Move: down");
   }
-  if(key === MOVE_RIGHT_KEY) {
+  if (key === MOVE_RIGHT_KEY) {
     connection.write("Move: right");
   }
-  if(key === '1') {
+  if (key === '1') {
     connection.write("Say: Yo!");
   }
-  if(key === '2') {
+  if (key === '2') {
     connection.write("Say: Haha!");
   }
-  if(key === '3') {
+  if (key === '3') {
     connection.write("Say: Seeya nerd!");
   }
-}
+};
 
 module.exports = { setupInput };
